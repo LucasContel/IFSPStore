12cfe3f88f1e61ce936c04fa24cb7033e389ea6d
LucasContel <l.contel@aluno.ifsp.edu.br>
2024-11-05 11:32:41 -0300

    Vlast

---
+++ IFSPStore.Repository/Mapping/GrupoMap.cs
@@ -0,0 +1,22 @@
+﻿using IFSPStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IFSPStore.Repository.Mapping
+{
+    public class GrupoMap : IEntityTypeConfiguration<Grupo>
+    {
+        public void Configure(EntityTypeBuilder<Grupo> builder)
+        {
+            builder.ToTable("Something");
+
+            builder.HasKey(prop => prop.Id);
+
+            builder.Property(prop => prop.Nome)
+                .HasColumnName("SomethingElse")
+                .IsRequired()
+                .HasMaxLength(50);
+                //.HasColumnType("varchar(50)");
+        }
+    }
+}
